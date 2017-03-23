082eb88a7dbd0f14f5bedf2a21eb7cbf0cbf431e
Reinhard Merfeld <reinhard.merfeld@commerzbank.com>
2017-03-23 10:45:40 +0100

    date component

--- simple-app/src/app/app.module.ts
+++ simple-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DateComponent } from './date/date.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateComponent
   ],
   imports: [
     BrowserModule,
